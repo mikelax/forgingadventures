3fc77f823d82ab4a0a9c3296bea5ce71daf6fe58
Nazar Aziz <mcnazar@gmail.com>
2017-12-05 14:34:18 +0000

    remove relations for now

--- api/models/game.js
+++ api/models/game.js
@@ -4,16 +4,4 @@ import GameMessage from 'models/gameMessage';
 
 export default class Game extends Model {
   static tableName = 'games';
-
-  static relationMappings = {
-    messages: {
-      relation: Model.HasManyRelation,
-      modelClass: GameMessage,
-      join: {
-        from: 'Game.id',
-        to: 'GameMessage.gameId'
-      }
-    }
-  };
-
 }
