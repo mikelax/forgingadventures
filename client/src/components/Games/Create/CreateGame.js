ec8621d01c32178f2c276793769280893c098d19
Nazar Aziz <mcnazar@gmail.com>
2017-12-06 20:36:36 +0000

    #2 - wip - Implement react Form select on create/edit game screen

--- client/src/components/Games/Create/CreateGame.js
+++ client/src/components/Games/Create/CreateGame.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Button, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
@@ -8,10 +9,20 @@ import { createGameMutation, gamesQuery } from '../queries';
 const CreateGame = class CreateGame extends Component {
 
   state = {
-    minPlayers: 1,
-    maxPlayers: 10,
-    skillLevel: 1,
-    postingFrequency: 1
+    // the form control state
+    store: {
+      title: '',
+      scenario: '',
+      overview: '',
+      gameSettings: {
+        minPlayers: 2,
+        maxPlayers: 6,
+        skillLevel: 1,
+        postingFrequency: 1
+      }
+    },
+    // the form validity state
+    errors: {}
   };
 
   render() {
@@ -24,97 +35,136 @@ const CreateGame = class CreateGame extends Component {
         <h1>Create a New Campaign</h1>
 
         <form>
-          <FormGroup>
+          <FormGroup validationState={this.validity('title')}>
             <ControlLabel>Campaign Name</ControlLabel>
             <FormControl
               type="text"
-              value={this.state.title}
+              value={this.formValue('title')}
               placeholder="Enter Campaign Name"
-              onChange={e => this.setState({ title: e.target.value })}
+              onChange={this.formInput('title')}
             />
           </FormGroup>
 
-          <FormGroup>
+          <FormGroup validationState={this.validity('scenario')}>
             <FormControl
               componentClass="textarea"
-              value={this.state.scenario}
+              value={this.formValue('scenario')}
               placeholder="Enter Scenario"
-              onChange={e => this.setState({ scenario: e.target.value })}
+              onChange={this.formInput('scenario')}
             />
+          </FormGroup>
 
+          <FormGroup validationState={this.validity('overview')}>
             <FormControl
               componentClass="textarea"
-              value={this.state.overview}
+              value={this.formValue('overview')}
               placeholder="Enter Overview"
-              onChange={e => this.setState({ overview: e.target.value })}
+              onChange={this.formInput('overview')}
             />
           </FormGroup>
 
           <FormGroup>
             <ControlLabel>Minimum Players</ControlLabel>
             <FormControl
-              type="text"
-              value={this.state.minPlayers}
+              type="number"
+              value={this.formValue('gameSettings.minPlayers')}
               placeholder="Minimum Players"
-              onChange={e => this.setState({ minPlayers: e.target.value })}
+              onChange={this.formInput('gameSettings.minPlayers')}
             />
             <ControlLabel>Max Players</ControlLabel>
             <FormControl
-              type="text"
-              value={this.state.maxPlayers}
+              type="number"
+              value={this.formValue('gameSettings.maxPlayers')}
               placeholder="Maximum Players"
-              onChange={e => this.setState({ maxPlayers: e.target.value })}
+              onChange={this.formInput('gameSettings.maxPlayers')}
             />
+            
             <ControlLabel>Skill Level</ControlLabel>
             <FormControl
-              type="text"
-              value={this.state.skillLevel}
-              placeholder="Minimum Level"
-              onChange={e => this.setState({ skillLevel: e.target.value })}
-            />
+              componentClass="select"
+              value={this.formValue('gameSettings.skillLevel')}
+              onChange={this.formInput('gameSettings.skillLevel')}>
+              <option value="1">Any/Newbie friendly</option>
+              <option value="2">I’ve rolled dice before</option>
+              <option value="3">Expert/role play master and rules bookworm</option>
+            </FormControl>
+            
             <ControlLabel>Posting Frequence</ControlLabel>
             <FormControl
-              type="text"
-              value={this.state.postingFrequency}
-              placeholder="Posting Frequence"
-              onChange={e => this.setState({ postingFrequency: e.target.value })}
-            />
+              componentClass="select"
+              value={this.formValue('gameSettings.postingFrequency')}
+              onChange={this.formInput('gameSettings.postingFrequency')}>
+              <option value="1">About 1 / day</option>
+              <option value="2">2-3 times / week</option>
+              <option value="3">Hardcore - More than 1 / day</option>
+            </FormControl>
           </FormGroup>
         </form>
 
         <Button bsStyle="primary" onClick={this.submit}>Submit</Button>
       </div>
     );
-  }
+  };
 
   submit = () => {
-    this.props
-      .mutate({
-        variables: {
-          input: {
-            title: this.state.title,
-            scenario: this.state.scenario,
-            overview: this.state.overview,
-            gameSettings: {
-              minPlayers: this.state.minPlayers,
-              maxPlayers: this.state.maxPlayers,
-              skillLevel: this.state.skillLevel,
-              postingFrequency: this.state.postingFrequency,
-            }
+    if (this.valid()) {
+      this.props
+        .mutate({
+          variables: {
+            input: this.state.store
+          },
+          update: (store, { data: { createGame } }) => {
+            //update: (store, props) => {
+            // Read the data from our cache for this query.
+            const data = store.readQuery({ query: gamesQuery });
+            // Add our game from the mutation to the end.
+            data.games.push(createGame);
+            // Write our data back to the cache.
+            store.writeQuery({ query: gamesQuery, data });
           }
-        },
-        update: (store, { data: { createGame } }) => {
-        //update: (store, props) => {
-          // Read the data from our cache for this query.
-          const data = store.readQuery({ query: gamesQuery });
-          // Add our game from the mutation to the end.
-          data.games.push(createGame);
-          // Write our data back to the cache.
-          store.writeQuery({ query: gamesQuery, data });
-        }
-      })
-      .then(() => this.setState({ saved: true }))
-  }
+        })
+        .then(() => this.setState({ saved: true }))
+    }
+  };
+
+  valid = () => {
+    this.setState({...this.state, errors: {}});
+
+    this.setError('title', 'Title is required');
+    this.setError('scenario', 'Scenario is required');
+    this.setError('overview', 'Overview is required');
+
+    return _.keys(this.state.errors).length === 0;
+  };
+
+  validity = (field) => {
+    if (this.state.errors[field]) {
+      return 'error';
+    }
+  };
+
+  setError = (field, message) => {
+    console.log('this.state.errors', field, _.isEmpty(this.formInput(field)), JSON.stringify(this.state.errors))
+    //_.isEmpty(this.formInput(field)) && (this.state.errors[field] = message);
+    _.isEmpty(this.formInput(field)) && this.setState({...this.state, errors: {
+        ...this.state.errors,
+        [field]: message
+      }});
+  };
+
+
+
+  formInput = (stateKey) => {
+    return (e) => {
+      _.set(this.state.store, stateKey, e.target.value);
+      //force update since we've mutated the state directly
+      this.forceUpdate();
+    }
+  };
+
+  formValue = (stateKey) => {
+    return _.get(this.state.store, stateKey, '');
+  };
 };
 
 export default graphql(createGameMutation)(CreateGame);
