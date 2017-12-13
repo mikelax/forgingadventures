4324c0d9ac19a61e014b4f9468405e35c6077c1a
Nazar Aziz <mcnazar@gmail.com>
2017-12-13 20:26:50 +0000

    refs #7 - various
    1. add sample styling for shout, sing and think
    2. add toggle logic to toolbar

--- client/src/components/Games/components/GameMessage/GameMessage.js
+++ client/src/components/Games/components/GameMessage/GameMessage.js
@@ -4,8 +4,15 @@ import {CompositeDecorator, convertFromRaw, convertToRaw, Editor, EditorState, E
 import {getSelectionEntity} from 'draftjs-utils';
 
 import 'draft-js/dist/Draft.css';
-import './GameMessage.css';
+import './assets/GameMessage.css';
 
+import iconMusic from './assets/icon-music-note.svg';
+
+/*
+GamesMessage is export wrapper component around draft js
+
+This module also contains GamesMessage sub-components - i.e. toolbar and inline and block style definitions
+ */
 
 export default class GamesMessage extends Component {
 
@@ -71,7 +78,56 @@ export default class GamesMessage extends Component {
 
 };
 
-/// private implementation details
+/// private GamesMessage methods - i.e. handlers etc...
+
+function onEditorChange(editorState) {
+  this.setState({editorState});
+}
+
+function onToggleAction(entityKey) {
+  const editorState = this.state.editorState;
+  const currentContent = editorState.getCurrentContent();
+  const selection = editorState.getSelection();
+  const selectionEntity = getSelectionEntity(editorState);
+  const collapsed = selection.isCollapsed();
+
+  // many many many options here
+  // 1. caret is selecting a range with no entity - apply entity
+  if (!collapsed && !selectionEntity) {
+    const textWithEntity = Modifier.applyEntity(currentContent, selection, entityKey);
+    onEditorChange.call(this, EditorState.push(editorState, textWithEntity, 'apply-entity'));
+  }
+  // 2. caret contains selection and the selection matches the entityKey - remove entity
+  else if (!collapsed && selectionEntity === entityKey) {
+    const textWithEntity = Modifier.applyEntity(currentContent, selection, null);
+    onEditorChange.call(this, EditorState.push(editorState, textWithEntity, 'apply-entity'));
+  }
+  // 3. caret contains selection and the selection doesn't matches the entityKey - remove entity and apply new one
+  else if (!collapsed && selectionEntity !== entityKey) {
+    const textWithEntity = Modifier.applyEntity(currentContent, selection, entityKey);
+    onEditorChange.call(this, EditorState.push(editorState, textWithEntity, 'apply-entity'));
+  }
+  // 4. caret is collapsed (i.e. no selection) and a new style is being applied - insert new style
+  else if (collapsed && selectionEntity !== entityKey) {
+    const textWithEntity = Modifier.insertText(currentContent, selection, ' ', null, entityKey);
+    onEditorChange.call(this, EditorState.push(editorState, textWithEntity, 'insert-characters'));
+  }
+  // 5. caret is collapsed (i.e. no selection) and the same style is being applied - insert a break
+  // FIXME - this is a bit iffy - think about this one
+  else if (collapsed && selectionEntity === entityKey) {
+    const textWithEntity = Modifier.insertText(currentContent, selection, ' ', null, null);
+    onEditorChange.call(this, EditorState.push(editorState, textWithEntity, 'apply-entity'));
+  }
+  // we shouldn't get here!11
+  else {
+    throw 'boom! boom!';
+  }
+}
+
+
+/*
+ ActionControls is the toolabr component, which is built based on styles defined in buttons
+ */
 
 const ActionControls = (props) => {
   const {editorState} = props;
@@ -87,21 +143,32 @@ const ActionControls = (props) => {
           label={button.label}
           onToggle={props.onToggle}
           type={button.type}
+          icon={button.icon}
         />
       )}
     </div>
   );
 };
 
+
+/*
+ActionButton is the toolbar button
+ */
+
+
 class ActionButton extends React.Component {
   render() {
+    const icon = this.props.icon ?
+      <img src={this.props.icon} alt={this.props.label}/>
+      : this.props.label;
+
     return (
       <button
         className={this.props.active ? 'active' : ''}
         type="button"
         title={this.props.type}
         onMouseDown={this._toggle}>
-        {this.props.label}
+        {icon}
       </button>
     );
   }
@@ -111,6 +178,10 @@ class ActionButton extends React.Component {
     this.props.onToggle(this.props.entity);
   };
 }
+
+/*
+define individual styles as functional components
+*/
 
 const ThinkSpan = (props) => {
   return (
@@ -130,7 +201,9 @@ const ShoutSpan = (props) => {
 const SingSpan = (props) => {
   return (
     <span className="singing" data-offset-key={props.offsetkey}>
+      <img src={iconMusic} />
       {props.children}
+      <img src={iconMusic} />
     </span>
   );
 };
@@ -164,22 +237,10 @@ const buttons = [
   {
     label: 'S',
     type: 'SING',
+    icon: iconMusic,
     entity: Entity.create('SING', 'MUTABLE')
   }
 ];
-
-function onEditorChange(editorState) {
-  this.setState({editorState});
-}
-
-function onToggleAction(entityKey) {
-  const editorState = this.state.editorState;
-  const currentContent = editorState.getCurrentContent();
-  const selection = editorState.getSelection();
-  const textWithEntity = Modifier.applyEntity(currentContent, selection, entityKey);
-
-  onEditorChange.call(this, EditorState.push(editorState, textWithEntity, 'apply-entity'));
-}
 
 function findEntitiesByType(type) {
   return function (contentBlock, callback, contentState) {
