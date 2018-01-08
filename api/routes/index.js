d1dd48ca0358ef967ac8529e0fc748fd7c1c34c7
Michael Holtzman <mholtzman@gmail.com>
2018-01-07 22:56:42 -0500

    #32 WIP for profile image upload.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,21 @@
-import { checkAuth0Secret } from 'middleware/security';
-import postUsers from './usersImpl';
+import multer from 'multer';
+
+import { checkAuth0Secret, checkJwt } from 'middleware/security';
+import { postUsers, uploadUserPicture } from './usersImpl';
 
 const router = require('express').Router();
+
+const uploader = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5000000 } // limit max upload file to 5MB
+});
 
 router.get('/', (req, res) => {
   res.send('API Index');
 });
 
 router.post('/users', checkAuth0Secret(), postUsers());
+router.post('/users/profile/image', checkJwt(), uploader.single('picture'), uploadUserPicture());
 
 
 export default router;
--- api/routes/usersImpl.js
+++ api/routes/usersImpl.js
@@ -1,7 +1,12 @@
 import _ from 'lodash';
+import aws from 'aws-sdk';
+import Bluebird from 'bluebird';
+import cloudinary from 'cloudinary';
+import config from 'config';
+
 import User from 'models/user';
 
-export default function postUsers() {
+export function postUsers() {
   return (req, res, next) => {
     const user = _.pick(req.body.data, ['auth0UserId', 'userMetadata', 'appMetadata']);
 
@@ -35,4 +40,69 @@ export default function postUsers() {
         next(error);
       });
   };
+}
+
+export function uploadUserPicture() {
+  // req.file.
+  return (req, res, next) => {
+    // TODO create random filename
+    const fileName = 'somerandomfilename';
+
+    Bluebird.all([
+      new Bluebird((resolve, reject) => {
+        cloudinary.config({
+          cloud_name: config.get('cloudinary.cloudName'),
+          api_key: config.get('cloudinary.apiKey'),
+          api_secret: config.get('cloudinary.apiSecret')
+        });
+
+        cloudinary.v2.uploader.upload_stream({
+          public_id: fileName,
+          resource_type: 'auto',
+          tags: 'profile,user'
+        }, (error, result) => {
+          if (error) {
+            return reject(error);
+          }
+
+          return resolve(result);
+        })
+          .end(req.file.buffer);
+      }),
+      s3UploadFile(req.file.buffer, fileName)
+    ])
+      .spread((cloudinaryResult, s3Result) => {
+        console.log(JSON.stringify(cloudinaryResult));
+        console.log(JSON.stringify(s3Result));
+
+        // TODO write record to files table in db
+
+        res.json({ cloudinaryId: cloudinaryResult.public_id });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  };
+}
+
+function s3UploadFile(fileBuffer, fileName) {
+  // Only need to set key and secret in dev, othewise will grab from ECS IAM role
+  aws.config.setPromisesDependency(Bluebird);
+  console.log(`the env is:  ${process.env.NODE_ENV}`);
+  // TODO should get NODE_ENV env var set
+  // if (process.env.NODE_ENV === 'development') {
+    aws.config.update({
+      accessKeyId: config.get('aws.config.accessKeyId'),
+      secretAccessKey: config.get('aws.config.secretAccessKey')
+    });
+  // }
+  const s3 = new aws.S3();
+  const options = {
+    Body: fileBuffer,
+    Bucket: config.get('aws.s3.userUploadsBucket'),
+    Key: `${config.get('aws.s3.userUploadsPrefex')}/${fileName}`
+  };
+
+  return s3.upload(options)
+    .promise();
 }
