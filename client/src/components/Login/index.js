26007a4a40f44117ccd94e77c5affc314a99616c
Michael Holtzman <mholtzman@gmail.com>
2018-02-15 00:50:12 -0500

    63 Add authguard to almost finished route.

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
 
 import AlmostFinished from './AlmostFinished';
+import AuthGuard from '../shared/components/AuthGuard';
 import Callback from './Callback';
 import Login from './Login';
 
@@ -14,7 +15,7 @@ export default class LoginView extends Component {
       <div className="login">
         <Switch>
           <Route path={`${match.url}/callback`} component={Callback}/>
-          <Route path={`${match.url}/almost-finished`} component={AlmostFinished}/>
+          <Route path={`${match.url}/almost-finished`} component={AuthGuard(AlmostFinished)}/>
           <Route exact path={`${match.url}/`} component={Login}/>
         </Switch>
       </div>
