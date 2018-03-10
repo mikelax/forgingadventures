0103d5db57699cb6b06cba6a97e753c462a5e494
Michael Holtzman <mholtzman@gmail.com>
2018-03-10 10:34:04 -0500

    #97 update UI tab layout for players tab. Use table for nicer layout.

--- client/src/components/Games/View/index.js
+++ client/src/components/Games/View/index.js
@@ -84,7 +84,7 @@ function gameDetails() {
       <CreateMessage gameId={game.id}/> :
         <Message info
           header='Please Login or Join Game to Post'
-          content='While games are viewable by everyone, only players are able to post game messages. Try posting a message in the Lounge, or searching for another open game.'
+          content='While games are viewable by everyone, only players are able to post Game Messages. Try posting a message in the Game Lounge, or searching for another open game.'
         />;
 
       return (
