cfd97d2ed8eb53c6f520ea8fad3a5d842877b1e5
Nazar Aziz <mcnazar@gmail.com>
2018-04-12 11:45:25 +0100

    refs #115 - more fields

--- client/src/components/Characters/components/CharacterLabelForms/CharacterDetails5e_1_0_0.js
+++ client/src/components/Characters/components/CharacterLabelForms/CharacterDetails5e_1_0_0.js
@@ -91,15 +91,15 @@ export default class CharacterDetails5e_1_0_0 extends Component {
 
             <Form.Group widths="equal">
               <Form.Field required>
-                <label>Current</label>
+                <Form.Field required label="Current" name="health.currentHitPoints" control={Field} type="number" min="0" />
               </Form.Field>
 
               <Form.Field required>
-                <label>Max</label>
+                <Form.Field required label="Max" name="health.maxHitPoints" control={Field} type="number" min="1" />
               </Form.Field>
 
               <Form.Field required>
-                <label>Hit Die</label>
+                <Form.Field required label="Hit Die" name="health.hitDie" control={Field} type="number" min="0" />
               </Form.Field>
             </Form.Group>
 
@@ -108,23 +108,141 @@ export default class CharacterDetails5e_1_0_0 extends Component {
             <Form.Group widths="equal">
               <Form.Field required>
                 <label>Background</label>
+                <Field
+                  name="background"
+                  component="select"
+                >
+                  <option value="background1">back1</option>
+                </Field>
               </Form.Field>
 
               <Form.Field required>
                 <label>Sex</label>
+                <Field
+                  name="sex"
+                  component="select"
+                >
+                  <option value="female">Female</option>
+                  <option value="male">Male</option>
+                  <option value="other">Other</option>
+                </Field>
               </Form.Field>
             </Form.Group>
 
             <Form.Group>
               <Form.Field required>
                 <label>Physical Characteristics</label>
+                <Field name="physicalCharacteristics" type="textarea"/>
               </Form.Field>
               <Form.Field required>
                 <label>Features and Traits</label>
+                <Field name="featuresTraits" type="textarea"/>
               </Form.Field>
             </Form.Group>
 
             <h3>Abilities</h3>
+
+            <Form.Group widths="equal">
+              <div>
+                <h4>Strength</h4>
+                <Form.Field required>
+                  <label>Base</label>
+                  <Field name="strength.baseValue" type="number" min="1"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Race Bonus</label>
+                  <Field name="strength.raceBonus" type="number" min="0"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Modifier</label>
+                  <Field name="strength.modifier" type="number" min="0" readonly />
+                </Form.Field>
+              </div>
+
+              <div>
+                <h4>Dexterity</h4>
+                <Form.Field required>
+                  <label>Base</label>
+                  <Field name="dexterity.baseValue" type="number" min="1"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Race Bonus</label>
+                  <Field name="dexterity.raceBonus" type="number" min="0"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Modifier</label>
+                  <Field name="dexterity.modifier" type="number" min="0" readonly />
+                </Form.Field>
+              </div>
+
+              <div>
+                <h4>Constitution</h4>
+                <Form.Field required>
+                  <label>Base</label>
+                  <Field name="constitution.baseValue" type="number" min="1"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Race Bonus</label>
+                  <Field name="constitution.raceBonus" type="number" min="0"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Modifier</label>
+                  <Field name="constitution.modifier" type="number" min="0" readonly />
+                </Form.Field>
+              </div>
+
+            </Form.Group>
+
+            <Form.Group widths="equal">
+              <div>
+                <h4>Inelligence</h4>
+                <Form.Field required>
+                  <label>Base</label>
+                  <Field name="intelligence.baseValue" type="number" min="1"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Race Bonus</label>
+                  <Field name="intelligence.raceBonus" type="number" min="0"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Modifier</label>
+                  <Field name="intelligence.modifier" type="number" min="0" readonly />
+                </Form.Field>
+              </div>
+
+              <div>
+                <h4>Wisdom</h4>
+                <Form.Field required>
+                  <label>Base</label>
+                  <Field name="wisdom.baseValue" type="number" min="1"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Race Bonus</label>
+                  <Field name="wisdom.raceBonus" type="number" min="0"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Modifier</label>
+                  <Field name="wisdom.modifier" type="number" min="0" readonly />
+                </Form.Field>
+              </div>
+
+              <div>
+                <h4>Charisma</h4>
+                <Form.Field required>
+                  <label>Base</label>
+                  <Field name="charisma.baseValue" type="number" min="1"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Race Bonus</label>
+                  <Field name="charisma.raceBonus" type="number" min="0"/>
+                </Form.Field>
+                <Form.Field>
+                  <label>Modifier</label>
+                  <Field name="charisma.modifier" type="number" min="0" readonly />
+                </Form.Field>
+              </div>
+
+            </Form.Group>
 
           </Form>
         )}
