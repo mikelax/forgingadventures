aa7800020f8569a5155a3548154a9d1813680775
Nazar Aziz <mcnazar@gmail.com>
2018-05-28 14:08:39 +0100

    #132 - tweak migrations (and fix an old one)

--- api/migrations/20180504102539_127_add_post_type_to_game_messages.js
+++ api/migrations/20180504102539_127_add_post_type_to_game_messages.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     table.integer('characterId');
     table.foreign('characterId').references('characters.id');
 
-    table.enu('postType', ['gm', 'ic', 'ooc']);
+    table.enu('postType', ['ic', 'ooc']);
   })
     .then(() => (
       knex('game_messages')
