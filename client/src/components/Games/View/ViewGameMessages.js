a68e3bfc5df2bd03ea7c0a7501f1dfa3f181f3ca
Nazar Aziz <mcnazar@gmail.com>
2018-06-13 11:39:27 +0100

    #145 - Error viewing Messages page with no character selected for player

--- client/src/components/Games/View/ViewGameMessages.js
+++ client/src/components/Games/View/ViewGameMessages.js
@@ -15,16 +15,6 @@ import { gameQuery, myGamePlayerQuery } from '../queries';
 function ViewGameMessages(props) {
   const { data: { game } } = props;
   const myGamePlayer = _.get(props, 'myGamePlayerQuery.myGamePlayer');
-  const playerOrGm = !_.isEmpty(myGamePlayer) && _.some(myGamePlayer, (value) => {
-    return _.includes(['game-master', 'accepted'], value.status);
-  });
-
-  const editorBlock = playerOrGm
-    ? <CreateMessage gameId={game.id}/>
-    : <Message info
-             header='Please Login or Join Game to Post'
-             content='While games are viewable by everyone, only players are able to post Game Messages. Try posting a message in the Game Lounge, or searching for another open game.'
-    />;
 
   return (
     <React.Fragment>
@@ -33,7 +23,7 @@ function ViewGameMessages(props) {
       <GamesMessages gameId={game.id}/>
 
       <Segment>
-        {editorBlock}
+        <EditorBlock myGamePlayer={myGamePlayer} game={game}/>
       </Segment>
     </React.Fragment>
   );
@@ -50,3 +40,52 @@ export default compose(
   ApolloLoader,
   pure,
 )(ViewGameMessages);
+
+
+function EditorBlock(props) {
+  const { myGamePlayer, game } = props;
+
+  const isGm = _.some(myGamePlayer, (value) => {
+    return _.includes(['game-master'], value.status);
+  });
+
+  const isPlayerWithCharacter = _.some(myGamePlayer, (value) => {
+    return _.includes(['accepted'], value.status) &&
+      !(_.isEmpty(value.character));
+  });
+
+  const isPlayerWithoutCharacter = _.some(myGamePlayer, (value) => {
+    return _.includes(['accepted'], value.status) &&
+      _.isEmpty(value.character);
+  });
+
+  const showCreateMessage = isGm || isPlayerWithCharacter;
+
+  if (showCreateMessage) {
+    return <CreateMessage gameId={game.id}/>;
+  } else if (isPlayerWithoutCharacter) {
+    return <AddCharacterMessage />;
+  } else {
+    return <JoinGameMessage />;
+  }
+}
+
+function AddCharacterMessage() {
+  return (
+    <Message
+      info
+      header='Please assign a character to post Game Messages'
+      content='You can view existing Game Messages but can only create a Game Message once you have chosen a character.'
+    />
+  );
+}
+
+function JoinGameMessage() {
+  return (
+    <Message
+      info
+      header='Please Login or Join Game to Post'
+      content='While games are viewable by everyone, only players are able to post Game Messages. Try posting a message in the Game Lounge, or searching for another open game.'
+    />
+  );
+}
