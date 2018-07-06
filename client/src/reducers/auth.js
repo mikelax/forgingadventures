4aca2fb533338b21227bcae0137f1e8d1c3bed13
Nazar Aziz <mcnazar@gmail.com>
2018-07-06 18:04:57 +0100

    closes #170 - Refactor logout logic to take into consideration auth0 hosted login page

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,5 @@
-import { AUTH_FAILURE, AUTH_SUCCESS, LOGOUT_SUCCESS } from '../actions/auth';
-import { isAuthenticated } from '../services/login';
+import { AUTH_FAILURE, AUTH_SUCCESS } from 'actions/auth';
+import { isAuthenticated } from 'services/login';
 
 export default function authReducer(state = {
   isAuthenticated: isAuthenticated(),
@@ -20,12 +20,6 @@ export default function authReducer(state = {
         loading: false,
         isAuthenticated: false,
         error: action.error
-      };
-    case LOGOUT_SUCCESS:
-      return {
-        ...state,
-        isAuthenticated: false,
-        accessToken: null
       };
     default:
       return state;
